99e6f31acdd1ea6f70208ade6018c30a35460431
Victor Gomes <vitor_gomis98@hotmail.com>
2018-07-30 21:00:51 -0300

    Alteração no nome do arquivo form-login.php -> index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once("banco/conecta.php");
+require_once("usuario.php");
+
+mostraAlerta("success");
+mostraAlerta("danger");
+?>
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+
+    <div class="container">
+        <img src="imagens/logo.png" class="img-fluid rounded mx-auto d-block" id="logo">    
+    </div>
+    <div class="container col-3">
+        <form action="login.php" method="post">
+            <div class="form-group">            
+                <input type="text" class="form-control" placeholder="Usuário" name="user">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Senha" name="password">
+            </div>            
+            <input type="submit" class="btn btn-primary btn-block" value="Entrar"></input>
+            <button class="btn btn-link btn-block btn-sm" value="Recuperar Senha">Recuperar Senha </button>
+        </form>
+    </div>    
+    <?php require_once("footer.php"); ?>
+
